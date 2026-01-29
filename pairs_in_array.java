d9ed67d83f8e5e196c60a4def67bbfc1598ad131
Rahul Mudgal <rahulmudgal886@gmail.com>
2026-01-29 22:26:16 +0530

    Added program to print array pairs.

---
+++ pairs_in_array.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class pairs_in_array {
+
+    public static void pair(int number[]) {
+        for(int i=0; i<number.length; i++) {
+            int current = number[i];
+            for(int j=i+1; j<number.length; j++) {
+                System.out.print("(" + current + "," + number[j] + ")");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]) {
+        int numbers[] = {2, 4, 6, 8, 10};
+        pair(numbers);
+    }
+}
